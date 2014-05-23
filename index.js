c366ea57fe4a327fd16aafb383db8ac28b88e258
dotch <cweiss@mozilla.com>
2014-05-23 12:39:54 -0700

    added insertion time and the ability to sort results by insertion time. added comments.

--- index.js
+++ index.js
@@ -38,7 +38,9 @@
       };
       req.onupgradeneeded = function (e) {
         self.db = req.result;
-        self.db.createObjectStore(self.storeName, { keyPath: 'key' });
+        var store = self.db.createObjectStore(self.storeName, {keyPath: 'key'});
+        store.createIndex('insertionTime', 'insertionTime');
+        store.createIndex('value', 'value');
         resolve(self);
       };
       req.onerror = reject;
@@ -52,6 +54,8 @@
 
   KeyValueStore.prototype = {
 
+    // Internal function: returns the objectStore with the supplied 
+    // transaction mode. Defaults to readonly transaction.
     _getObjectStore: function(mode) {
       var self = this;
       mode = typeof mode !== 'undefined' ? mode : 'readonly';
@@ -59,6 +63,8 @@
       return t.objectStore(self.storeName);
     }, 
 
+    // Internal function to defer the execution of a supplied function
+    // until the database is ready.
     _awaitReady: function(fn, args) {
       var self = this;
       if (self._ready) {
@@ -70,9 +76,14 @@
       }
     },
 
+    /**
+     * Get the value for a given key.
+     * @param  {string|number} key
+     * @return {promise}
+     */
     get: function (key) {
       var self = this;
-      return self._awaitReady(self._get,[key]);
+      return self._awaitReady(self._get, arguments);
     },
 
     _get: function (key) {
@@ -85,20 +96,35 @@
       });
     },
 
+    /**
+     * Set the value for a given key.
+     * @param {string|number} key
+     * @param {string|number|object} value
+     * @return {promise}
+     */
     set: function (key, value) {
       var self = this;
-      return self._awaitReady(self._set,[key,value]);
+      return self._awaitReady(self._set, arguments);
     },
 
     _set: function (key, value) {
       var self = this;
       var store = self._getObjectStore('readwrite');
-      return wrap(store.put({ 'key': key, 'value': value }));
+      return wrap(store.put({
+        'key': key,
+        'value': value,
+        'insertionTime': Date.now()
+      }));
     },
 
+    /**
+     * Removes the the entry with the supplied key from the database.
+     * @param  {string|number} key
+     * @return {promise}
+     */
     remove: function (key) {
       var self = this;
-      return self._awaitReady(self._remove,[key]);
+      return self._awaitReady(self._remove, arguments);
     },
 
     _remove: function (key) {
@@ -107,60 +133,75 @@
       return wrap(store.delete(key));
     },
 
-    getAll: function() {
+    /**
+     * Returns all databse entries.
+     * @param  {string}  [orderBy='key'] The index to order the results by.
+     *                                   Can be 'key', 'value' or 'insertionTime'.
+     * @param  {boolean} [reverse=false] Reverse the order of the results.
+     * @return {promise}
+     */
+    getAll: function(orderBy, reverse) {
       var self = this;
-      return self._awaitReady(self._getAll);
+      return self._awaitReady(self._getAll, arguments);
     },
 
-    _getAll: function() {
+    _getAll: function(orderBy, reverse) {
       var self = this;
-      var store = self._getObjectStore();
-      var allItems = [];
-      return new Promise(function(resolve,reject){
-        var cursorRequest = store.openCursor();
-        cursorRequest.onsuccess = function(e){
-          var cursor = e.target.result;
-          if (cursor === null || cursor === undefined) {
-            resolve(allItems);
-          } else {
-            allItems.push(cursor.value);
-            cursor.continue();
-          }
-        }
+      return new Promise(function (resolve,reject) {
+        // Get all entries by calling _getRange with the count of all 
+        // database entries.
+        self._size().then(function(e){
+          resolve(self._getRange(e,orderBy,null,reverse));
+        });
       });
     },
 
-    getRange: function(count, startKey) {
+    /**
+     * Returns a range of database entries.
+     * @param  {number}  count
+     * @param  {string}  [orderBy='key'] The index to order the results by.
+     *                                   Can be 'key', 'value' or 'insertionTime'.
+     * @param  {any}     [start]         The start entry of the results.
+     *                                   Dependent on the selected index.
+     * @param  {boolean} [reverse=false] Reverse the order of the results.
+     * @return {promise}
+     */
+    getRange: function(count, orderBy, start, reverse) {
       var self = this;
-      return self._awaitReady(self._getRange, [count, startKey]);
+      return self._awaitReady(self._getRange, arguments);
     },
 
-    _getRange: function(count, startKey) {
-      console.log(arguments);
+    _getRange: function(count, orderBy, start, reverse) {
       var self = this;
       var store = self._getObjectStore();
       var allItems = [];
+      var counter = 0;
+      var lowerBound = start ? IDBKeyRange.lowerBound(start) : null;
+      var direction = reverse ? 'prev' : 'next';
       return new Promise(function(resolve,reject){
-        var index = 0;
-        if (startKey) {
-          var lowerBound = IDBKeyRange.lowerBound(startKey);
-          var cursorRequest = store.openCursor(lowerBound);
+        if (orderBy === 'value' || orderBy === 'insertionTime') {
+          var index = store.index(orderBy);
+          var cursorRequest = index.openCursor(lowerBound, direction);          
         } else {
-          var cursorRequest = store.openCursor();
-        };
+          var cursorRequest = store.openCursor(lowerBound, direction);
+        }
         cursorRequest.onsuccess = function(e){
           var cursor = e.target.result;
-          if (cursor === null || cursor === undefined || index >= count) {
+          if (cursor === null || cursor === undefined || counter >= count) {
             resolve(allItems);
           } else {
             allItems.push(cursor.value);
-            index++;
+            counter++;
             cursor.continue();
           }
         }
       });
     },
 
+    /**
+     * Returns the number of database entries.
+     * @return {promise}
+     */
     size: function() {
       var self = this;
       return self._awaitReady(self._size);
@@ -172,6 +213,10 @@
       return wrap(store.count());
     },
 
+    /**
+     * Deletes all database entries.
+     * @return {promise}
+     */
     clear: function () {
       var self = this;
       return self._awaitReady(self._clear);
@@ -207,8 +252,8 @@ var StoragePrototype = Object.create(HTMLElement.prototype);
   StoragePrototype.remove = function (key) {
     return this.storage.remove(key);
   };
-  StoragePrototype.getAll = function (key) {
-    return this.storage.getAll();
+  StoragePrototype.getAll = function (orderBy, reverse) {
+    return this.storage.getAll(orderBy, reverse);
   };
   StoragePrototype.size = function (key) {
     return this.storage.size();
@@ -216,8 +261,8 @@ var StoragePrototype = Object.create(HTMLElement.prototype);
   StoragePrototype.clear = function (key) {
     return this.storage.clear();
   };
-  StoragePrototype.getRange = function (count, startKey) {
-    return this.storage.getRange(count, startKey);
+  StoragePrototype.getRange = function (count, orderBy, start, reverse) {
+    return this.storage.getRange(count, orderBy, start, reverse);
   };
 
   document.registerElement('key-value', {
